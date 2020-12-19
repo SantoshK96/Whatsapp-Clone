50b13d030c09b6027f6a85bc50794b84bfc53aaf
SantoshK96 <santoshkumarp1996@gmail.com>
2020-12-19 13:19:10 +0530

    first commit : sidebar implementation & pending chat

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDT0Jsd3vxE2d69HEN0McLzC-UijDDjUKk",
+    authDomain: "whatsapp-clone-3356a.firebaseapp.com",
+    projectId: "whatsapp-clone-3356a",
+    storageBucket: "whatsapp-clone-3356a.appspot.com",
+    messagingSenderId: "1003333119467",
+    appId: "1:1003333119467:web:43a9218647ddb0df473dae",
+    measurementId: "G-SLZDQ89Z0N"
+  };
