2a3ad9c644d4c479de48c5f712743a155b35013d
SantoshK96 <santoshkumarp1996@gmail.com>
2020-12-19 21:44:20 +0530

    Firebase DB connection, add new chat enabled

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,10 +1,19 @@
+import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDT0Jsd3vxE2d69HEN0McLzC-UijDDjUKk",
-    authDomain: "whatsapp-clone-3356a.firebaseapp.com",
-    projectId: "whatsapp-clone-3356a",
-    storageBucket: "whatsapp-clone-3356a.appspot.com",
-    messagingSenderId: "1003333119467",
-    appId: "1:1003333119467:web:43a9218647ddb0df473dae",
-    measurementId: "G-SLZDQ89Z0N"
-  };
+  apiKey: "AIzaSyDT0Jsd3vxE2d69HEN0McLzC-UijDDjUKk",
+  authDomain: "whatsapp-clone-3356a.firebaseapp.com",
+  projectId: "whatsapp-clone-3356a",
+  storageBucket: "whatsapp-clone-3356a.appspot.com",
+  messagingSenderId: "1003333119467",
+  appId: "1:1003333119467:web:43a9218647ddb0df473dae",
+  measurementId: "G-SLZDQ89Z0N"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
