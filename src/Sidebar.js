dfb45d92f528de0293e11052e8dc5061fc0481ea
SantoshK96 <santoshkumarp1996@gmail.com>
2020-12-20 10:53:46 +0530

    Changing room name dynamically on click of chats from sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,13 +12,16 @@ import db from "./firebase";
 function Sidebar() {
     const [rooms, setRooms] = useState([]);
     useEffect(() => {
-        db.collection('rooms').onSnapshot(snapshot => {
+        const unsubscribe = db.collection('rooms').onSnapshot(snapshot => {
             setRooms(snapshot.docs.map(doc => ({
                 id: doc.id,
                 data: doc.data(),
             })))
         })
-    }, [])
+        return () => {
+            unsubscribe();
+        }
+    }, []);
 
     return (
         <div className="sidebar">
